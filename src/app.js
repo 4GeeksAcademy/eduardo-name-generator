c126d8bb63169e0ec0d86caf85baf41e3210eb7f
EduardoGP <124112815+Eduardo7052311@users.noreply.github.com>
2024-11-28 02:03:26 +0000

    actualizacion generador nombres de dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  pronoun.forEach(articulo => {
+    adj.forEach(adj => {
+      noun.forEach(suj => {
+        console.log(articulo + adj + suj + ".com");
+      });
+    });
+  });
   console.log("Hello Rigo from the console!");
 };
